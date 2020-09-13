4aff032d56bd60cc90c508278b78d905edfbbfad
chenrun <17610780919@163.com>
2020-09-13 21:07:21 +0800

    增加从c++中获取java方法

--- Android逆向/NDK开发详解/Refelect/app/src/main/cpp/native-lib.cpp
+++ Android逆向/NDK开发详解/Refelect/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,20 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_refelect_MainActivity_stringFromJNI(
-        JNIEnv* env,
+        JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    jclass TestJclass = env->FindClass("com/example/refelect/Test");
+    jfieldID publicStaticField_jfieldID = env->GetStaticFieldID(TestJclass, "publicStaticField",
+                                                                "Ljava/lang/String;");
+    jstring publicStaticField_content = (jstring) env->GetStaticObjectField(TestJclass,
+                                                                            publicStaticField_jfieldID);
+    const char *content_ptr = env->GetStringUTFChars(publicStaticField_content, nullptr);
+//    int __android_log_print(int prio, const char* tag, const char* fmt, ...)
+    __android_log_print(4, "kanxue->jni", "jni->%s", content_ptr);
+
     return env->NewStringUTF(hello.c_str());
 }
